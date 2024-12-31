ba86a86d276c1a49388fc1d1011264d0eb2f20fa
Taha Ghadiyali <tahaghadiyali07@gmail.com>
2024-12-31 21:51:12 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Hi! This is my machine, It's a bit old but does the job perfectly :D 
+    Also, I'm currently working on some ml models and plan to implement some research papers by myself.
+    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+  "cs-degree": `I'm a 2nd year electronics undergrad interested in ML and embedded systems, I'm currently learning and looking forward to collab and work together with people of similar interest :D`,
+  "sofa-table": `That's my sofa. I like to brainstorm and overthink here when I'm not working. (I also occasionally eat here, don't tell my mom about it XD)`,
+  tv: `I've like watching content centered around Astrophysics and Electronics, these are my go to channels :
+   <a href="https://www.youtube.com/@whatdamath target="_blank">Anton Petrov</a>, <a href="https://www.youtube.com/@EEVblog" target="_blank">EEVblog</a>,
+  <a href="https://www.youtube.com/@w2aew" target="_blank">w2aew</a> and <a href="https://www.youtube.com/@Vsauce" target="_blank">Vsauce</a>! (Personal Favourite)`,
+  bed: `I'm a sleepyhead, can't help myself with that haha..`,
+  resume: `Looking forward to working on some interesting projects :) Contact me at <a href="https://x.com/kc9va" target="_blank">X</a> .`,
+  projects: `I'm working on a few projects, I'll update this section when I'm done.`,
+  library: `I'm planning to buy some books, welcoming all the suggestions!`,
+  exit: `Close the tab to exit, see you around!`,
+};
